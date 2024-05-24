c71c9b8b3393de03b440840056a9d5eee61a9c87
ericliao <chixiang777@gmail.com>
2024-05-24 18:21:43 +0800

    feat: 新增頁籤畫面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import MainView from '../views/MainView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,8 +17,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/main',
+      name: 'main',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: MainView
     }
   ]
-})
+});
 
-export default router
+export default router;
---
+++ src/views/MainView.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+const currentTab = ref('清單');
+
+onMounted(() => {});
+</script>
+<template>
+  <div>
+    <h3 class="mb-4">假日驗證</h3>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :class="{
+            active: currentTab === '清單',
+            'text-light': currentTab === '清單',
+            'text-dark': currentTab !== '清單'
+          }"
+          aria-current="page"
+          href="#"
+          @click="
+            {
+              currentTab = '清單';
+            }
+          "
+          >清單</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :class="{
+            active: currentTab === '查詢',
+            'text-light': currentTab === '查詢',
+            'text-dark': currentTab !== '查詢'
+          }"
+          href="#"
+          @click="
+            {
+              currentTab = '查詢';
+            }
+          "
+          >查詢</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :class="{
+            active: currentTab === '上傳檔案',
+            'text-light': currentTab === '上傳檔案',
+            'text-dark': currentTab !== '上傳檔案'
+          }"
+          href="#"
+          @click="
+            {
+              currentTab = '上傳檔案';
+            }
+          "
+          >上傳檔案</a
+        >
+      </li>
+    </ul>
+    <hr />
+    <div></div>
+  </div>
+</template>
+<style></style>
