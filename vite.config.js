9b543ca0d32f7f7c3c602a0d569a0ec03b20d00e
ericliao <chixiang777@gmail.com>
2024-05-24 15:27:00 +0800

    feat: 新增環境變數設定 1. 加入.env及.env.development 2. vite.config.js設定proxy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const vite_env = env.VITE_ENV;
+  // 設定proxy
+  let proxy;
+  if (vite_env === 'development') {
+    proxy = {
+      '/holidayVerification_baseUrl': {
+        target: 'https://localhost:7002',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/holidayVerification_baseUrl/, ''),
+        secure: false
+      }
+    };
+  } else {
+    proxy = {};
   }
-})
+
+  return {
+    build: {
+      emptyOutDir: true
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: proxy
+    }
+  };
+});
