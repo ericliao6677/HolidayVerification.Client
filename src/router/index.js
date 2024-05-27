6e04a103eda10d66ea8c7fd34baca99ac818e409
ericliao <chixiang777@gmail.com>
2024-05-27 16:18:40 +0800

    update: 修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'main',
+      component: MainView
     },
     {
       path: '/about',
@@ -17,14 +17,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/main',
-      name: 'main',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: MainView
     }
   ]
 });
